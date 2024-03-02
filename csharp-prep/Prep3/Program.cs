334c93e8f05867c0d381f9ba3d5512c8bfaa2d0e
helamancarrillo <helamancarrillo11@gmail.com>
2024-03-01 20:59:24 -0600

    prep 3 first week done!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,84 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+
+        
+
+        /*Console.Write("Enter the magic number: ");
+        string magic = Console.ReadLine();
+        int magicNumber = int.Parse(magic);*/
+
+        int guessNumber = -1; 
+
+
+        while (guessNumber != magicNumber )
+        {
+            Console.Write("Enter your guess: ");
+            string guess = Console.ReadLine();
+            guessNumber = int.Parse(guess); 
+
+            string answer = "";
+
+
+            if (guessNumber < magicNumber)
+
+            {
+               //Console.WriteLine("Higher");
+                answer = "Higher";
+            }
+            else if (guessNumber > magicNumber)
+            {
+                //Console.WriteLine("Lower");
+                answer = "Lower";
+            }
+            else if (guessNumber == magicNumber)
+            {
+                //Console.WriteLine("You guessed it!");
+                answer = "You guessed it!";
+            }
+    
+            Console.WriteLine($"{answer}");
+    
+             
+
+        }   
+
+        
+
+    
+        
+
+
+       
+
+        
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
