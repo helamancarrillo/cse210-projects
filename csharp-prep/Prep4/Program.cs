b93d20d2c737fbb2b972ed70e46648cbafcf59b8
helamancarrillo <helamancarrillo11@gmail.com>
2024-03-01 23:30:20 -0600

    prep 4 first week done!

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,75 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int toAddNumber = -1; 
+
+        List<int> words = new List<int>();
+
+         
+
+        while (toAddNumber != 0)
+        {
+
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+            toAddNumber = int.Parse(userInput);
+
+
+            words.Add(toAddNumber);
+
+        
+
+        }
+
+        int sum = words.Sum();
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = (float)sum / words.Count;
+
+        Console.WriteLine($"The average is: {average}");
+
+        int largest = words.Max();
+
+        Console.WriteLine($"The largest number is: {largest}");
+
+
+
+
+
+
+        /*foreach (int word in words)
+            {
+                Console.WriteLine(word);
+            }  */
+
+         
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
+   
+
+
 }
