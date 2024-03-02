76b4c847ab6b282a47896828b60b7ebfc35a42c6
helamancarrillo <helamancarrillo11@gmail.com>
2024-03-01 18:48:53 -0600

    Prep 2 first week done!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,72 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int userGrade = int.Parse(userInput); 
+
+        string letter = "";
+
+        if (userGrade >= 90)
+        {
+            letter = "A";
+            //Console.WriteLine("Letter grade: A");
+            
+        }
+        else if (userGrade >= 80)
+        {
+            letter = "B";
+            //Console.WriteLine("Letter grade: B");
+            
+        }
+         else if (userGrade >= 70)
+        {
+            letter = "C";
+            //Console.WriteLine("Letter grade: C");
+            
+        }
+         else if (userGrade >= 60)
+        {
+            letter = "D";
+            //Console.WriteLine("Letter grade: D");
+           
+        }
+         else if (userGrade < 60)
+        {
+            letter = "F";
+            //Console.WriteLine("Letter grade: F");
+            
+        }
+
+        Console.WriteLine($"Letter grade: {letter}");
+        
+
+
+        
+
+        
+
+
+        if (userGrade >= 70)
+        {
+            Console.Write("Congratulations, you did it!");
+        }
+        else
+        {
+            Console.Write("Try next time!");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
